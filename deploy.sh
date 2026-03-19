07a00375f7a16cc36a8593e5a81f6cb747882ce9
Benoit Lorriette <benoit.lorriette@probance.com>
2026-03-19 17:03:19 +0900

    override pipeline v9

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 RSYNC_PARAM="-avz --itemize-changes --dry-run --delete --delete-excluded --exclude='.*' --exclude='*.md' --exclude='deploy.sh' --exclude='docker-compose.yml' --exclude='Dockerfile' --exclude='README.md' --exclude='docs' --exclude='data'"
 
 echo "Debug:"
-echo "rsync ${RSYNC_PARAM} . \"${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}\""
-rsync ${RSYNC_PARAM} . "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}"
+echo "rsync ${RSYNC_PARAM} ./ \"${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}/\""
+rsync ${RSYNC_PARAM} ./ "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}/"
