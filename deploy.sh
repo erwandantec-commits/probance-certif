4765e5cfbb9607921e6e95b08c22917ab47a5035
Benoit Lorriette <benoit.lorriette@probance.com>
2026-03-19 17:11:36 +0900

    override pipeline v14

--- deploy.sh
+++ deploy.sh
@@ -24,7 +24,7 @@ rsync ${RSYNC_PARAM[@]} . "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_PATH}"
 
 # Run remote command
 echo "Recreate containers"
-ssh "${REMOTE_USER}@${REMOTE_HOST}" << "EOF"
+ssh "${REMOTE_USER}@${REMOTE_HOST}" << EOF
 set -e
 cd "${REMOTE_PATH}"
 echo "Working dir: \$(pwd)"
